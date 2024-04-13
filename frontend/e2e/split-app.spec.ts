bdd6c2ad20bb163874d018959cb24213ebb96481
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2024-04-13 17:03:07 +0900

    add e2e testing

--- frontend/e2e/split-app.spec.ts
+++ frontend/e2e/split-app.spec.ts
@@ -31,4 +31,44 @@ test.describe("割り勘アプリ", () => {
     await expect(page.getByText("メンバーは2人以上必要です")).toBeVisible();
     await expect(page).not.toHaveURL(/.+\/group\/group1/);
   });
+
+  test.describe("支出登録機能", () => {
+    test.beforeEach(async ({ page }) => {
+      const groupNameInput = page.getByLabel("グループ名");
+      await groupNameInput.fill("group1");
+      const memberListInput = page.getByLabel("メンバー");
+      await memberListInput.fill("太郎, 花子");
+
+      const submitButton = page.getByRole("button");
+      await submitButton.click();
+    });
+
+    test("支出が登録され生産リストが更新される", async ({ page }) => {
+      const expenseNameInput = page.getByLabel("支出名");
+      await expenseNameInput.fill("ランチ");
+      const amountInput = page.getByLabel("金額");
+      await amountInput.fill("1000");
+      const memberSelect = page.getByLabel("支払うメンバー");
+      await memberSelect.selectOption("花子");
+
+      const submitButton = page.getByRole("button");
+      await submitButton.click();
+
+      await expect(page).toHaveURL(/.+\/group\/group1/);
+      await expect(page.getByRole("list")).toHaveText("太郎 → 花子500円");
+    });
+
+    test("バリデーションエラーが存在する場合支出が登録されず生産リストが登録されない", async ({
+      page,
+    }) => {
+      const submitButton = page.getByRole("button");
+      await submitButton.click();
+
+      await expect(page.getByText("支出名は必須です")).toBeVisible();
+      await expect(
+        page.getByText("金額は1円以上の整数で必須です")
+      ).toBeVisible();
+      await expect(page.getByRole("list")).toHaveText("");
+    });
+  });
 });
